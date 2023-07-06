0e0446b420129874d5c5894b1cda8a648c201f8f
tienviet10 <viettran101294@gmail.com>
2023-07-05 23:43:22 -0400

    Set up example for testing.

---
+++ src/main/java/com/viettran/mockito/business/SomeBusinessImpl.java
@@ -0,0 +1,25 @@
+package com.viettran.mockito.business;
+
+public class SomeBusinessImpl {
+    private DataService dataService;
+
+    public SomeBusinessImpl(DataService dataService) {
+        super();
+        this.dataService = dataService;
+    }
+
+    public int findTheGratestFromAllData() {
+        int[] data = dataService.retrieveAllData();
+        int greatest = Integer.MIN_VALUE;
+        for (int value : data) {
+            if (value > greatest) {
+                greatest = value;
+            }
+        }
+        return greatest;
+    }
+}
+
+interface DataService {
+    int[] retrieveAllData();
+}
---
+++ src/test/java/com/viettran/mockito/business/SomeBusinessImplTest.java
@@ -0,0 +1,22 @@
+package com.viettran.mockito.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeBusinessImplTest {
+    @Test
+    void test(){
+        DataServiceStub dataServiceStub = new DataServiceStub();
+        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
+        int result = business.findTheGratestFromAllData();
+        assertEquals(24, result);
+    }
+}
+
+class DataServiceStub implements DataService {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[] {24, 15, 3};
+    }
+}
